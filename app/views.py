a6d7cb403a029c5cbccd7aa390c618a97250d4a6
UETOPIA <ed@uetopia.com>
2023-06-23 23:29:37 -0600

    login testing

--- app/views.py
+++ app/views.py
@@ -268,7 +268,7 @@ def me():
 
         if this_wallet['userid'] == "0":
             print('user id set to zero')
-            auth_token = this_wallet.encode_auth_token(this_wallet['_id'])
+            auth_token = lightning_wallet_model.encode_auth_token(this_wallet['_id'])
             if auth_token:
                 responseObject = {
                     'userid': 0,
@@ -284,7 +284,7 @@ def me():
             user = user_model.find_by_id(ObjectId(this_wallet['userid']))
             if user == None:
                 print('user not found')
-                auth_token = this_wallet.encode_auth_token(this_wallet['_id'])
+                auth_token = lightning_wallet_model.encode_auth_token(this_wallet['_id'])
                 if auth_token:
                     responseObject = {
                         'userid': 0,
@@ -298,7 +298,7 @@ def me():
             else:
                 print('found user')
                 ## generate an auth token
-                auth_token = this_wallet.encode_auth_token(this_wallet['_id'])
+                auth_token = lightning_wallet_model.encode_auth_token(this_wallet['_id'])
                 if auth_token:
                     responseObject = {
                         'userid': user['_id'],
